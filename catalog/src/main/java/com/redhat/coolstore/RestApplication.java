9125406eb0f0339b4f53d4235830d17f2ea29af6
Eric Deandrea <eric.deandrea@gmail.com>
2019-10-29 10:59:42 -0400

    Update to Spring Boot 2/Spring Cloud Greenwich.SR3 (#5)

--- catalog/src/main/java/com/redhat/coolstore/RestApplication.java
+++ catalog/src/main/java/com/redhat/coolstore/RestApplication.java
@@ -2,7 +2,7 @@ package com.redhat.coolstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
